d7835e8af5d5aea7ad4bc852b9365c71c7eccf38
HughP01 <71641914+HughP01@users.noreply.github.com>
2026-03-01 11:18:56 +0000

    Update and rename

---
+++ finance loggr.py
@@ -0,0 +1,11 @@
+import base64
+import io
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+from dash import Dash, dcc, html, Input, Output, State, dash_table, callback_context
+import dash
+
+# --- APP ---
+app = Dash(__name__, suppress_callback_exceptions=True)
+app.title = "finance loggr"
